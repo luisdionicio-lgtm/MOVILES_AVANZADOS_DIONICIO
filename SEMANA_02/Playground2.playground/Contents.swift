c32a2c016fdf6935f8defec646ed9ed91dde8922
LuisDB <luis.dionicio@tecsup.edu.pe>
2026-08-27 12:28:44 -0500

    Agregar ingreso de datos

---
+++ SEMANA_02/Playground2.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+// Definir el promedio por pesos
+let parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+// Ingreso de datos
+print("Nombre del Alumno:")
+let alumno = readLine() ?? ""
+
+print("Nota del Examen Parcial:")
+let exParcial = Double(readLine() ?? "") ?? 0
+
+print("Nota del Trabajo:")
+let notaTrabajo = Double(readLine() ?? "") ?? 0
+
+print("Nota del Examen Final:")
+let exFinal = Double(readLine() ?? "") ?? 0
